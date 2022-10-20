3466540b4c256fe8efa93a0e587308007fc2e481
aliyenurerdogu <l1911012059@ogr.sdu.edu.tr>
2022-10-20 20:56:36 +0300

    last gonderim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,17 +1,72 @@
-var express = require('express');
-var router = express.Router();
-
 const anaSayfa = function(req,res){
-    res.render('anasayfa', { "title": "Anasayfa"});
-
+    res.render('anasayfa', 
+    { 
+    "baslik": "Anasayfa",
+    "sayfaBaslik":{
+        "siteAd":"MekanBul",
+        "slogan":"Civardaki Mekanları Kesfet"
+    },
+    "mekanlar":[
+        {
+            "ad": "starbucks",
+            "puan":"3",
+            "adres":"centrum garden avm",
+            "imkanlar":["dünya kahveleri","pastalar","kekler"],
+            "mesafe":"10km"
+                    
+        },
+        { 
+            "ad": "gloria jeans",
+            "puan":"1",
+            "adres":"SDÜ Doğu Kampüsü",
+            "imkanlar":["cay","kek","kahve"],
+            "mesafe":"5km"
+        }   
+    ]
+});
 }
 const mekanBilgisi = function(req,res){
-    res.render('mekanbilgisi', { "title": "Mekan Bilgisi" });
-
+    res.render('mekanbilgisi',
+     {
+        "baslik": "Mekan Bilgisi",
+        "mekanBaslik" : "Starbucks",
+        "mekanDetay":{
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden avm",
+            "saatler":[
+                {
+                    "gunler":"pazartesi-cuma",
+                    "acilis":"9:00",
+                    "kapanis":"23:00",
+                    "kapali": false
+                },
+                {
+                    "gunler":"cumartesi-pazar",
+                    "acilis":"8:00",
+                    "kapanis":"22:00",
+                    "kapali": false
+                }
+            ],
+            "imkanlar":["kahve","çay","kek"],
+            "koordinatlar":
+            {
+                "enlem":"37.7",
+                "boylam":"30.5"
+            },
+            "yorumlar":[
+                {
+                    "yorumYapan":"Asım Sinan",
+                    "yorumMetini":"berbaaat",
+                    "tarih":"20 ekim 2022",
+                    "puan":"1"
+                }
+            ]
+        }
+});
 }
 const yorumEkle=function(req,res,){
     res.render('yorumekle', { "title":"Yorum Sayfası" });
-
 };
 
 module.exports = {
